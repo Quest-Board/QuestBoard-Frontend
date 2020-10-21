b00fa84360d42eacf43cbffca68aab401c17f110
brent <bhbrain@iastate.edu>
2020-10-21 16:22:27 -0700

    Added new files to Login.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,19 +1,56 @@
 import React, { Component } from "react";
+import {Redirect} from "react-router-dom";
 
 export default class Login extends Component {
+    password;
+    address;
+    constructor(){
+        super();
+        this.state={redirect:null};
+    }
+    LoginCall=(event)=>{
+        event.preventDefault();
+        const Http = new XMLHttpRequest();
+        const url='https://jsonplaceholder.typcode.com/posts';
+        const data={
+            email: this.address,
+            password: this.password
+        }
+        Http.open("POST",url);
+        Http.send(JSON.stringify(data));
+        if(this.address==="Test@Test.com"&&this.password==="TestPass1"){
+            this.setState({redirect: "/Board"});
+        }
+        else{
+            alert("Password or Username is incorrect");
+        }
+
+    }
+
+    UserChangeHandler=(event)=>{
+        this.address=event.target.value;
+    }
+
+    PassChangeHandler=(event)=>{
+        this.password=event.target.value;
+    }
+
     render() {
+        if(this.state.redirect){
+            return <Redirect to={this.state.redirect}/>
+        }
         return (
-            <form>
+            <form onSubmit={this.LoginCall}>
                 <h3>Sign In</h3>
 
                 <div className="form-group">
                     <label>Email address</label>
-                    <input type="email" className="form-control" placeholder="Enter email" />
+                    <input type="email" onChange={this.UserChangeHandler} className="form-control" placeholder="Enter email" />
                 </div>
 
                 <div className="form-group">
                     <label>Password</label>
-                    <input type="password" className="form-control" placeholder="Enter password" />
+                    <input type="password" onChange={this.PassChangeHandler} className="form-control" placeholder="Enter password" />
                 </div>
 
                 <div className="form-group">
@@ -23,7 +60,7 @@ export default class Login extends Component {
                     </div>
                 </div>
 
-                <button type="submit" className="btn btn-primary btn-block">Submit</button>
+                <button type="submit" className="btn btn-primary btn-block" >Submit</button>
                 <p className="forgot-password text-right">
                     Forgot <a href="#">password?</a>
                 </p>
