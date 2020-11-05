28fc523377e3942bb8098dde8a68d137bf263401
brent <bhbrain@iastate.edu>
2020-11-05 14:43:00 -0800

    Added Columns and Cards to Board

---
+++ src/components/Card.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+export default class Card extends Component {
+
+
+
+    render() {
+        return (
+            <div>
+                Hello World
+            </div>
+        );
+    }
+}
