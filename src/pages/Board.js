01d8359de2b4381450e11800df3073023f24b610
brent <bhbrain@iastate.edu>
2020-11-19 20:54:32 -0800

    Added Fetch

--- src/pages/Board.js
+++ src/pages/Board.js
@@ -26,7 +26,7 @@ export default class QuestBoard extends Component {
             referrerPolicy: 'no-referrer' // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-ur
         });
         console.log(response.json());
-        return response.json(); // parses JSON response into native JavaScript objects
+        //return response.json(); // parses JSON response into native JavaScript objects
     }
 
     //Board Handler Functions:
