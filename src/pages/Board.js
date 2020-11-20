52ca02dc78e232503b930d9f538ea35e0d7f4afe
crglick <crglick@iastate.edu>
2020-11-20 15:46:05 -0600

    added check for boards info in render

--- src/pages/Board.js
+++ src/pages/Board.js
@@ -160,6 +160,31 @@ export default class QuestBoard extends Component {
             return <Redirect to={this.state.redirect}/>
         }
 
+        if(!this.state.boardsInfo){
+            this.setState({
+                boardsInfo:[{
+                    lanes:[
+                        {
+                            id: 'lane1',
+                            title: 'Planned Tasks',
+                            label: '2/2',
+                            cards: [
+                                {id: 'Card1', title: 'Write Blog', description: 'Can AI make memes', label: '30 mins', draggable: true},
+                                {id: 'Card2', title: 'Pay Rent', description: 'Transfer via NEFT', label: '5 mins', draggable: true, metadata: {sha: 'be312a1'}}
+                            ]
+                        },
+                        {
+                            id: 'lane2',
+                            title: 'Completed',
+                            label: '0/0',
+                            cards: []
+                        }
+                    ],
+                    name:"Temp Board"
+                    }]
+            });
+        }
+
         return (
             <div className="Board">
                 <NavBar clickHandle={this.ChangeBoard} boardsInfo={this.state.boardsInfo} boardIndex={this.state.boardIndex}/>
