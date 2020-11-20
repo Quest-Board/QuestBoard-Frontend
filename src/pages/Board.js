143309c15d801cde19e42d0e2285d0e4b8378c08
brent <bhbrain@iastate.edu>
2020-11-19 22:26:55 -0800

    Try This Instead new

--- src/pages/Board.js
+++ src/pages/Board.js
@@ -27,25 +27,23 @@ export default class QuestBoard extends Component {
                 }
             ]}};
         console.log("beforeResponse1");
-        this.componentWillMount();
+        this.componentUpdate();
     }
-    async componentWillMount(){
+
+    async componentUpdate(){
         const response1= await this.postData("https://coms-319-t15.cs.iastate.edu/api/board/getboards")
         const json=await response1.json();
         this.setState({data:await json[0]});
-        console.log(json[0]);
     }
-    async postData(url = '', data = {}) {
+
+    async postData(url = '') {
         const response = await fetch(url, {
-            method: 'GET', // *GET, POST, PUT, DELETE, etc.
-            mode: 'cors', // no-cors, *cors, same-origin
-            cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-            credentials: 'same-origin', // include, *same-origin, omit
-            redirect: 'follow', // manual, *follow, error
-            referrerPolicy: 'no-referrer' // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-ur
+            method: 'GET',
+            mode: 'cors',
+            credentials: 'same-origin',
+            redirect: 'follow'
         })
-        //console.log(response.json());
-        return response; // parses JSON response into native JavaScript objects
+        return response;
     }
 
     //Board Handler Functions:
