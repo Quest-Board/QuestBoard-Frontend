7981c9ed43cf3c7e022e59570fad4306a5465dd8
brent <bhbrain@iastate.edu>
2020-11-19 23:56:03 -0800

    APIAddCard2

--- src/pages/Board.js
+++ src/pages/Board.js
@@ -56,7 +56,7 @@ export default class QuestBoard extends Component {
     }
 
     onCardAdd(card,laneId){
-        fetch("https://coms-319-t15.cs.iastate.edu/api/board/addcardtocolumn"),{
+        return fetch("https://coms-319-t15.cs.iastate.edu/api/board/addcardtocolumn"),{
             method: 'POST',
             mode: 'cors',
             credentials: 'same-origin',
